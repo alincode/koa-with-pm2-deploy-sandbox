712d801c0e0ff30e4469f99b636435dd6ae56ed8
alincode <alin.code@gmail.com>
2016-09-08 01:37:37 +0800

    change port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var koa = require('koa');
 var app = module.exports = koa();
 
-app.use(function *(){
+app.use(function*() {
   this.body = 'Hello World';
 });
 
-if (!module.parent) app.listen(3000);
+if (!module.parent) app.listen(80);
